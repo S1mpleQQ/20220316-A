d8b84abdf543248e1d870f07d3bc015488f05a4c
S1mpleQQ <c110196116@nkust.edu.tw>
2022-03-16 16:48:52 +0800

    加入專案檔案。

---
+++ 20220316-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220316_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入你的分數:");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 1000000 ? "及格" : "不及格";
+            Console.WriteLine("阿航，太可惜了，你{0}", result);
+            Console.ReadLine();
+
+        }
+    }
+}
